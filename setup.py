ce0a04a719acf4bfbc1f7f0a8d50fff6a259de20
Mike Smith <m.t.smith@sheffield.ac.uk>
2024-06-24 13:21:07 +0100

    Commandline tool for alignment

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'btalignment',
+  packages = ['btalignment'],
+  version = '1.0',
+  description = 'This computes the 3d pose of cameras and calibration boards.',
+  author = 'Mike Smith',
+  author_email = 'm.t.smith@sheffield.ac.uk',
+  url = 'https://github.com/SheffieldMLtracking/btalignment.git',
+  download_url = 'https://github.com/SheffieldMLtracking/btalignment.git',
+  keywords = ['3d','alignment','calibration','cameras','pose','position'],
+  classifiers = [],
+  install_requires=['numpy'],
+  scripts=['bin/btalignment'],
+)
