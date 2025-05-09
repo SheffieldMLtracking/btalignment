2ebe7bbdb63fda6e4de056083f97b35e815edf6c
Mike Smith <m.t.smith@sheffield.ac.uk>
2025-05-09 23:41:05 +0100

    Update setup.py
    fixing install requirements

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
   download_url = 'https://github.com/SheffieldMLtracking/btalignment.git',
   keywords = ['3d','alignment','calibration','cameras','pose','position'],
   classifiers = [],
-  install_requires=['numpy','pylibdmtx @ git+https://github.com/SheffieldMLtracking/pylibdmtx'],
+  install_requires=['numpy','pylibdmtx @ git+https://github.com/SheffieldMLtracking/pylibdmtx', 'alignment @ git+https://github.com/SheffieldMLtracking/alignment.git'],
   scripts=['bin/btalignment'],
 )
